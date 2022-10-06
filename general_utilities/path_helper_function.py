be7438506e0017afcaf05f2e9731966623197ec0
AlexLepauvre <54275933+AlexLepauvre@users.noreply.github.com>
2022-10-06 14:57:15 +0200

    Add function for mvpa analysis for object processing data set

--- general_utilities/path_helper_function.py
+++ general_utilities/path_helper_function.py
@@ -1,4 +1,36 @@
 import os
+import glob
+import numpy as np
+from pathlib import Path
+import mne
+
+
+def find_files(root, naming_pattern=None, extension=None):
+    """
+    This function finds files matching a specific naming pattern recursively in directory from root
+    :param root: root of the directory among which to search. Must be a string
+    :param naming_pattern: string the files must match to be returned
+    :param extension: Extension of the file to search for
+    :return:
+    """
+    if extension is None:
+        extension = '.*'
+    if naming_pattern is None:
+        naming_pattern = '*'
+
+    matches = []
+    for sub_folder, dirnames, filenames in os.walk(root):
+        for filename in glob.glob(sub_folder + os.sep + '*' + naming_pattern + '*' + extension):
+            matches.append(os.path.join(sub_folder, filename))
+    # XNAT will mess up the order of files in case there was an abortion, because it will put things in folders called
+    # ABORTED. Therefore, the files need to be sorted based on the file names:
+    # Getting the file names:
+    matches_file_names = [file.split(os.sep)[-1] for file in matches]
+    files_order = np.argsort(matches_file_names)
+    matches = [matches[ind] for ind in files_order]
+    [print(match) for match in matches]
+
+    return matches
 
 
 def list_subjects(root, prefix="sub-"):
@@ -13,3 +45,44 @@ def list_subjects(root, prefix="sub-"):
                     for folder in list_folders if prefix in folder]
 
     return subject_list
+
+
+def path_generator(root, analysis=None, preprocessing_steps=None, fig=False, results=False, data=False):
+    """
+    Generate the path to where the data should be saved
+    :param root: (string or pathlib path object) root of where the data should be saved
+    :param analysis: (string) name of the analysis. The highest level folder for the saving will be called accordingly
+    :param preprocessing_steps: (string) description of the preprocessing steps used to generate the used data to keep
+    track of things
+    :param fig: (boolean) whether or not the path is for saving figures. If set to false, the stats should be set to
+    true
+    :param results: (boolean) whether or not the path is for saving statistics. If set to false, the fig should be set
+    to true
+    :param data: (boolean) whether or not the path is for saving data. If set to false, the fig should be set
+    to true
+    :return: save_path (Pathlib path object) path to where the data should be saved
+    """
+
+    if fig is True and results is False and data is False:
+        save_path = Path(root, "figure", analysis, preprocessing_steps)
+    elif fig is False and results is True and data is False:
+        save_path = Path(root, "results", analysis, preprocessing_steps)
+    elif fig is False and results is False and data is True:
+        save_path = Path(root, "data", analysis, preprocessing_steps)
+    else:
+        raise Exception("You attempted to generate a path to save the analysis specifying that it'S neither stats nor "
+                        "figure. that doesn't work. Make sure that only one of the two is true")
+    # Generating the directory if that doesn't exist
+    if not os.path.isdir(save_path):
+        # Creating the directory:
+        os.makedirs(save_path)
+
+    return save_path
+
+
+def load_epochs(bids_root, subject, session, data_type, preprocessing_folder, signal, preprocessing_steps, task):
+    # Generate file name:
+    epoch_file = Path(bids_root, "derivatives", "preprocessing", subject, session, data_type,
+                      preprocessing_folder, signal, preprocessing_steps,
+                      "sub-{}_ses-{}_task-{}_desc-epoching_{}-epo.fif".format(subject, session, task, data_type))
+    return mne.read_epochs(epoch_file)
---
+++ object_perception_analysis/object_perception_analysis_master.py
@@ -0,0 +1,122 @@
+import argparse
+from pathlib import Path
+import os
+import json
+import mne
+import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from mne.decoding import (SlidingEstimator, GeneralizingEstimator, Scaler,
+                          cross_val_multiscore, LinearModel, get_coef,
+                          Vectorizer, CSP)
+import matplotlib.pyplot as plt
+
+from general_utilities.path_helper_function import find_files, list_subjects, path_generator, load_epochs
+
+
+def single_subject_mvpa(subject, epochs, config, conditions=None, labels_condition=None, classifier="svm", n_cv=5):
+    """
+
+    :param subject:
+    :param config:
+    :return:
+    """
+    # =========================================================================================
+    # Housekeeping
+    fig_save_root = path_generator(config["save_root"].format(subject), analysis=config["name"],
+                                   preprocessing_steps=config["preprocess_steps"],
+                                   fig=True, results=False, data=False)
+    results_save_root = path_generator(config["save_root"].format(subject), analysis=config["name"],
+                                       preprocessing_steps=config["preprocess_steps"],
+                                       fig=False, results=True, data=False)
+    # Saving the config where the data will be saved:
+    with open(Path(fig_save_root, 'config.json'), 'w') as f:
+        json.dump(config, f)
+    with open(Path(results_save_root, 'config.json'), 'w') as f:
+        json.dump(config, f)
+
+    # =========================================================================================
+    # Prepare the data:
+    if conditions is not None:
+        epochs = epochs[conditions]
+    # Extract the data:
+    data = np.squeeze(epochs.get_data())
+    # Extract the labels:
+    labels = epochs.metadata[labels_condition].to_numpy()
+
+    # Prepare the classifier:
+    if classifier.lower() == "svm":
+        clf = make_pipeline(
+            StandardScaler(),
+            LinearSVC()
+        )
+    elif classifier.lower() == "logisticregression":
+        clf = make_pipeline(
+            StandardScaler(),
+            LogisticRegression(solver='liblinear')
+        )
+    else:
+        raise Exception("The classifier passed was not recogized! Only SVM or logisticregression supported")
+
+    # Performing the decoding in a time resolved fashion:
+    time_decod = SlidingEstimator(clf, n_jobs=1, scoring='accuracy', verbose=True)
+    # Scoring the classifier:
+    scores = cross_val_multiscore(time_decod, data, labels, cv=n_cv, n_jobs=1)
+
+    # Saving the results to file:
+    scores.save(Path(fig_save_root, "sub-" + subject + "_decoding_scores.npy"))
+
+    # Average scores across cross-validation splits
+    scores = np.mean(scores, axis=0)
+    # Plot the results:
+    fig, ax = plt.subplots()
+    ax.plot(epochs.times, scores, label='score')
+    ax.axhline(.5, color='k', linestyle='--', label='chance')
+    ax.set_xlabel('Times')
+    ax.set_ylabel('Accuracy')  # Area Under the Curve
+    ax.legend()
+    ax.axvline(.0, color='k', linestyle='-')
+    ax.set_title('Sensor space decoding')
+    # Save the figure to a file:
+    plt.savefig(Path(fig_save_root, "sub-" + subject + "_decoding_scores.png"))
+    plt.close()
+    return scores
+
+
+def mvpa_manager():
+    parser = argparse.ArgumentParser(description="Arguments for MVPA analysis across all subjects")
+    parser.add_argument('--config', type=str, default=None,
+                        help="Config file for analysis parameters (file name + path)")
+    args = parser.parse_args()
+    if args.config is None:
+        configs = find_files(Path(os.getcwd(), "configs"), naming_pattern="*", extension=".json")
+    else:
+        configs = [args.config]
+
+    # Looping through all config:
+    for config in configs:
+        # Load the config:
+        config_file = open(config)
+        config = json.loads(config_file)[0]
+        # List the subjects:
+        subjects_list = list_subjects(Path(config["bids_root"], "derivatives", "preprocessing"), prefix="sub-")
+
+        # Looping through each subject to launch the analysis across all:
+        scores = []
+        for subject in subjects_list:
+            # Load this subject epochs:
+            epochs = load_epochs(config["bids_root"], "sub-" + subject,
+                                 "ses-" + config["ses"], config["data_type"], config["preprocess_folder"],
+                                 config["signal"], config["preprocess_steps"], config["task"])
+            # Run the decoding on this subject:
+            scores.append(single_subject_mvpa(subject, epochs, config,
+                                              conditions=config["conditions"],
+                                              labels_condition=config["labels_condition"],
+                                              classifier=config["classifier"],
+                                              n_cv=config["n_cv"]))
+
+
+if __name__ == "__main__":
+    mvpa_manager()
