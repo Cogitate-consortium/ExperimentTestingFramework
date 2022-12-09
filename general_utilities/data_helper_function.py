48a30291ce4e0331bfc9ebabb6115fcfbb3d1009
Alex Lepauvre <alex.lepauvre@ae.mpg.de>
2022-12-09 12:32:48 +0100

    New configs

--- general_utilities/data_helper_function.py
+++ general_utilities/data_helper_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import stats
+import statistics
 
 
 def mean_confidence_interval(data, confidence=0.95, axis=0):
@@ -18,3 +19,18 @@ def mean_confidence_interval(data, confidence=0.95, axis=0):
     m, se = np.mean(a, axis=axis), stats.sem(a, axis=axis)
     h = se * stats.t.ppf((1 + confidence) / 2., n - 1)
     return m, m - h, m + h
+
+
+def pooled_stdev(sample_1, sample_2):
+    """
+    This function computes the pooled standard deviation of two samples according to:
+    https://en.wikipedia.org/wiki/Pooled_variance
+    :param sample_1: (np array or list) first sample for which to compute the pooled variance
+    :param sample_2: (np array or list) first sample for which to compute the pooled variance
+    :return:
+    """
+    sd1 = statistics.stdev(sample_1)
+    sd2 = statistics.stdev(sample_2)
+    n1 = sample_1.shape[0]
+    n2 = sample_2.shape[0]
+    return np.sqrt(((n1 - 1) * sd1 * sd1 + (n2 - 1) * sd2 * sd2) / (n1 + n2 - 2))
