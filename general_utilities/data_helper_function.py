a7ccfd7b423661a2f85898f5c6073ed3d9943a35
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2023-12-18 09:08:59 +0100

    Clean up and document

--- general_utilities/data_helper_function.py
+++ general_utilities/data_helper_function.py
@@ -5,45 +5,22 @@ import mne
 import pandas as pd
 
 
-def mean_confidence_interval(data, confidence=0.95, axis=0):
-    """
-    This function computes the mean and the 95% confidence interval from the data, according to the axis.
-    :param data: (numpy array) data on which to compute the confidence interval and mean
-    :param confidence: (float) interval of the confidence interval: 0.95 means 95% confidence interval
-    :param axis: (int) axis from the data along which to compute the mean and confidence interval
-    :return:
-    mean (numpy array) mean of the data along specified dimensions
-    mean - ci (numpy array) mean of the data minus the confidence interval
-    mean + ci (numpy array) mean of the data plus the confidence interval
-    """
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a, axis=axis), sem(a, axis=axis)
-    h = se * t.ppf((1 + confidence) / 2., n - 1)
-    return m, m - h, m + h
-
-
-def pooled_stdev(sample_1, sample_2):
-    """
-    This function computes the pooled standard deviation of two samples according to:
-    https://en.wikipedia.org/wiki/Pooled_variance
-    :param sample_1: (np array or list) first sample for which to compute the pooled variance
-    :param sample_2: (np array or list) first sample for which to compute the pooled variance
-    :return:
-    """
-    sd1 = statistics.stdev(sample_1)
-    sd2 = statistics.stdev(sample_2)
-    n1 = sample_1.shape[0]
-    n2 = sample_2.shape[0]
-    return np.sqrt(((n1 - 1) * sd1 * sd1 + (n2 - 1) * sd2 * sd2) / (n1 + n2 - 2))
-
-
 def find_nearest_ind(array, value):
     return (np.abs(array - value)).argmin()
 
 
 def create_epochs(data, ch_names, ch_types, sfreq, conditions, times, n_trials_per_cond):
-    print("A")
+    """
+    This function creates an MNE epochs object.
+    :param data: (np array) trial x channel x time, data to populate the epochs with
+    :param ch_names: (list of strings) name of the channels. len(ch_names) must be equal to data.shape[1]
+    :param ch_types: (list of strings) type of each channel. len(ch_types) must be equal to data.shape[1]
+    :param sfreq: (int) sampling frequency of the signal
+    :param conditions: (string) name of the condition of that set of epochs
+    :param times: (np array) time vector of the epoch data
+    :param n_trials_per_cond: (int) number of trials per condition
+    :return:
+    """
     # Convert to an mne epochs (as this is what the different jittering functions require:
     ch_names = ch_names
     ch_types = [ch_types] * len(ch_names)
@@ -75,15 +52,17 @@ def create_epochs(data, ch_names, ch_types, sfreq, conditions, times, n_trials_p
 def compute_epochs_stat(epochs, tmin, tmax, metric="mean", channel="E77", cond_1="cond_1", cond_2="cond_2",
                         stat="tstat"):
     """
-
-    :param epochs:
-    :param tmin:
-    :param tmax:
-    :param metric:
-    :param channel:
-    :param cond_1:
-    :param cond_2:
-    :param stat:
+    This function computes the t-statistics of the difference between two conditions in a given time window on MNE
+    epochs data
+    :param epochs: (mne epoch object) contains the data on which to compute the t-stat
+    :param tmin: (float) start of time window
+    :param tmax: (float) end of time window in which to compute the t-stat
+    :param metric: (string) name of the metric to use to compute the aggregate in the time window. Can be "mean" or
+    "peak"
+    :param channel: (string or list) name of the channel to use to compute the data
+    :param cond_1: (string) name of the first condition
+    :param cond_2: (string) name of the second condition
+    :param stat: (string) name of the statistics to compute. Should be either t-stat or fsize
     :return:
     """
     if metric.lower() == "mean":
@@ -111,40 +90,11 @@ def compute_epochs_stat(epochs, tmin, tmax, metric="mean", channel="E77", cond_1
     return stat
 
 
-def epochs_fsize(epochs, tmin, tmax, metric="mean", channel="E77", cond_1="cond_1", cond_2="cond_2"):
-    """
-
-    :param epochs:
-    :param tmin:
-    :param tmax:
-    :param metric:
-    :param channel:
-    :param cond_1:
-    :param cond_2:
-    :return:
-    """
-    if metric.lower() == "mean":
-        func = np.mean
-    elif metric.lower() == "peak":
-        func = np.max
-
-    # Convert the data to data frame:
-    df_epochs = epochs.copy().pick(channel).crop(tmin=tmin, tmax=tmax).to_data_frame(long_format=True)
-    # Compute the dependent variable
-    dependent_var_df = \
-        df_epochs.groupby(['channel', 'epoch'], group_keys=False)[
-            "value"].aggregate(func).reset_index()
-    # Compute the t-statistic between both conditions:
-    stat, p_value = ttest_ind(dependent_var_df.loc[dependent_var_df["condition"] == cond_1, "value"],
-                              dependent_var_df.loc[dependent_var_df["condition"] == cond_2, "value"])
-    return stat
-
-
 def compute_t_stat(data_1, data_2, axis=0):
     """
     This function computes a zscore along a specific dimension of a matrix
-    :param data_2:
-    :param data_1:
+    :param data_1: (np array) data of the first condition
+    :param data_2: (np array) data of the second condition
     :param axis: (int) which axis along which to compute the zscore for the null distribution
     :return: zscore
     """
